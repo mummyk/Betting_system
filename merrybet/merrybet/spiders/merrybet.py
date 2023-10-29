f759ed9efb8ccdf1f32f6e493d5f07c1e0bd6e87
mummyk <chibuzoi410@yahoo.com>
2023-10-29 11:10:58 -0700

    Extracting categories merrybet

--- merrybet/merrybet/spiders/merrybet.py
+++ merrybet/merrybet/spiders/merrybet.py
@@ -1,3 +1,4 @@
+import json
 import scrapy
 
 
@@ -5,7 +6,15 @@ class Merrybet(scrapy.Spider):
     """Class for scraping merrybet data"""
     name = "merrybet"
     baseUrl = "https://m.merrybet.com"
+    #category = f'/rest/market/categories/{categoryId}/events'
+    #events =f'/rest/market/events/{eventId}'
     start_urls = [f'{baseUrl}/rest/market/categories']
 
-    def parse(self, response, **kwargs):
-        yield "done"
+    def parse(self, response):
+        data = json.loads(response.body)
+        # Extract the "categoryId" values
+        category_ids = [item["categoryId"] for item in data["data"]]
+
+        # Print the "categoryId" values
+        print("Category IDs:", category_ids)
+        yield data
